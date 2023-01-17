4e7b05b7ac3cfb1b1b4cb8fa6a95a80033d0c744
ibaikaa <ibaikaaaa@gmail.com>
2023-01-18 03:51:49 +0600

    Initial commit

---
+++ productsApp/MVC/View/OrderMethod/OrderMethodCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OrderMethodCollectionViewCell.swift
+//  productsApp
+//
+//  Created by ibaikaa on 18/1/23.
+//
+
+import UIKit
+
+class OrderMethodCollectionViewCell: UICollectionViewCell {
+    static let reuseID = String(describing: OrderMethodCollectionViewCell.self)
+}
